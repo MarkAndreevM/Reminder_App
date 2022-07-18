724dc6d7f5c03f95f9324dbe3f7888d33823a528
Mark <mark.kenny@yandex.ru>
2022-07-18 14:21:32 +0300

    edit reminder final

--- src/reminder/models.py
+++ src/reminder/models.py
@@ -7,12 +7,12 @@ class Notification(models.Model):
     def default_start_time():
         now = datetime.time(hour=8, minute=0)
         return now  
-    
+
     text_reminder = models.CharField(max_length=255)
     user_mail = models.EmailField(max_length=60)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_notification = models.DateField(auto_now=False)
-    time_notification = models.TimeField(default=default_start_time)
+    time_notification = models.CharField(max_length=16, default=default_start_time)
 
     def __str__(self):
         return f"Notification: {self.text_reminder}"
